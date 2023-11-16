bd03210f89d9bc92fa91fe6981c80e4d531f934e
Ndeye Marieme Aidara Ka <ndeyemariemeaidaraka@MacBook-Pro-de-Ndeye.local>
2023-11-16 16:03:52 +0100

    ficher

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express()
 const path = require('path');
 require('dotenv').config();
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'pug');
 
-const router = require(path.join(__dirname, 'app' , 'route.js'));
+//const router = require(path.join(__dirname, 'app' , 'route.js'));
+const router = require('./app/route.js');
+router(app);
 
-//const router = require('/app/routes.js');
 app.listen(process.env.PORT_HTTP,() => { 
     if(process.env.APP_ENV == 'dev') {
        console.log('serveur start on http://localhost:'+process.env.PORT_HTTP);
