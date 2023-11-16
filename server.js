68e0d3dc90725e7652882cc2b7a18af4306beabe
Ndeye Marieme Aidara Ka <ndeyemariemeaidaraka@MacBook-Pro-de-Ndeye.local>
2023-11-16 10:25:56 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+require('dotenv').config();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'pug');
+
+
+app.listen(process.env.PORT_HTTP,() => { 
+    if(process.env.APP_ENV == 'dev') {
+       console.log('serveur start on http://localhost:'+process.env.PORT_HTTP);
+    }
+   
+ })
