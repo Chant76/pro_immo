b21b17b6700a47f98fa1686ccb73265005d4ada0
Ndeye Marieme Aidara Ka <ndeyemariemeaidaraka@MacBook-Pro-de-Ndeye.local>
2023-11-17 10:50:07 +0100

    modif

--- app/route.js
+++ app/route.js
@@ -4,4 +4,9 @@ module.exports = (app) =>{
         let HomeController = require('../src/controllers/HomeController.js')
         HomeController.index(request,response)
     })
+
+    app.get('/inscript', (request, response) => {
+        let HomeController = require('../src/controllers/HomeController.js')
+        HomeController.inscript(request,response)
+    })
 };
--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -3,6 +3,10 @@ class HomeController{
     index (request, response) {
         response.render('index')
     }
+
+    inscript (request, response) {
+        response.render('inscript')
+    }
 };
 
 module.exports = new HomeController();
